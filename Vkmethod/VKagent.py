c435de3017ef98a58a15ccaf03cc148919a8ae45
sergryap <rs1180@mail.ru>
2022-06-28 19:56:55 +0500

    Decomposition handler_func, added step_2_func, step_other_func

--- Vkmethod/VKagent.py
+++ Vkmethod/VKagent.py
@@ -29,42 +29,49 @@ class VkAgent(VkSearch):
 							break
 
 	def handler_func(self, user_id, msg, result):
+		"""Функция-обработчик событий сервера типа MESSAGE_NEW"""
 		if result[5] == 1:
 			return self.get_data_user(user_id, msg)
 		if result[2] in [2, 3]:
 			return None, None, None, None, True, 1
 		if result[5] == 2:
-			user_info = result[0]
-			enter_age = result[1]
-			if enter_age and msg.isdigit():
-				age = int(msg)
-				self.update_year_birth(user_info, age)
-				return user_info, True, None, 0, False, 3
-			elif enter_age and not msg.isdigit():
-				self.send_message(user_id, "Введите верный возраст")
-				return user_info, True, None, 0, True, 2
-			else:
-				return user_info, True, None, 0, False, 3
+			return self.step_2_func(result, msg)
 		else:
-			user_info = result[0]
-			search_flag = result[1]
-			search_info = result[2]
-			step = result[3]
-			if search_flag:
-				search_info = self.users_search(user_info)
-				search_flag = False
-				return user_info, search_flag, search_info, step, False, None
-			else:
-				value = list(search_info.values())[step]
-				info = f"{value['first_name']} {value['last_name']}\n{value['url']}\n\n"
-				step += 1
-				self.send_message(user_id, info)
-				self.send_top_photos(value['user_id'], user_id)
-				self.send_message(user_id, "Для продолжения поиска введите любой символ")
-				if step == len(value) - 1:
-					step = 0
-					search_flag = True
-				return user_info, search_flag, search_info, step, True, None
+			return self.step_other_func(result, user_id)
+
+	def step_2_func(self, result, msg):
+		user_info = result[0]
+		enter_age = result[1]
+		if enter_age and msg.isdigit():
+			age = int(msg)
+			self.update_year_birth(user_info, age)
+			return user_info, True, None, 0, False, 3
+		elif enter_age and not msg.isdigit():
+			self.send_message(user_id, "Введите верный возраст")
+			return user_info, True, None, 0, True, 2
+		else:
+			return user_info, True, None, 0, False, 3
+
+	def step_other_func(self, result, user_id):
+		user_info = result[0]
+		search_flag = result[1]
+		search_info = result[2]
+		step = result[3]
+		if search_flag:
+			search_info = self.users_search(user_info)
+			search_flag = False
+			return user_info, search_flag, search_info, step, False, None
+		else:
+			value = list(search_info.values())[step]
+			info = f"{value['first_name']} {value['last_name']}\n{value['url']}\n\n"
+			step += 1
+			self.send_message(user_id, info)
+			self.send_top_photos(value['user_id'], user_id)
+			self.send_message(user_id, "Для продолжения поиска введите любой символ")
+			if step == len(value) - 1:
+				step = 0
+				search_flag = True
+			return user_info, search_flag, search_info, step, True, None
 
 	def get_data_user(self, user_id, msg):
 		"""
@@ -107,9 +114,9 @@ class VkAgent(VkSearch):
 			for photo in top_photo:
 				attachment += f"photo{user_id}_{photo[0]},"
 			self.vk_session.method("messages.send", {
-								"user_id": owner_id,
-								"attachment": attachment[:-1],
-								"random_id": 0})
+				"user_id": owner_id,
+				"attachment": attachment[:-1],
+				"random_id": 0})
 		elif top_photo:
 			self.send_message(owner_id, "Извините, но у пользователя закрытый профиль.\n Вы можете посмотреть фото по ссылкам")
 			for photo in top_photo:
